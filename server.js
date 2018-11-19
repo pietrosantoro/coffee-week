ede1b9e218003c17f3717017b28cce78945295c2
Pietro Santoro <pietrosantoro@MacBook-Pro-di-Pietro.local>
2018-11-19 12:25:06 +0000

    fixing folder

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/coffe-week'));
  
 app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
+  //Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname + '/dist/coffe-week/index.html'));
 });
 // Start the app by listening on the default Heroku port
